378e79ba02f60cba7e02839178dae486e508d295
samvit3 <65843078+samvit3@users.noreply.github.com>
2020-05-24 11:49:25 +0530

    adding more comment

--- classdemo/src/classdemo/calculator.java
+++ classdemo/src/classdemo/calculator.java
@@ -5,6 +5,7 @@ public class calculator {
 	public void display()
 	{
 		//hey satish I am adding some comments here
+		//hello man ..i am adding one more comment ..i like to write comments
 		System.out.println("hello");
 	}
 }
